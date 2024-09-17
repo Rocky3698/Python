b82ff915876377abdc703bd55f9a619c28261e52
Rocky Chowdhury <rocky20809@gmail.com>
2024-09-17 07:35:57 +0600

    connecting

---
+++ Vairable_DataType.py
@@ -0,0 +1,7 @@
+x = 10
+y = 3.14
+name = "Rocky"
+is_Student = True
+
+print(name,is_Student,x,y) # Rocky True 10 3.14
+print(type(name),type(is_Student),type(x), type(y)) #<class 'str'> <class 'bool'> <class 'int'> <class 'float'>
